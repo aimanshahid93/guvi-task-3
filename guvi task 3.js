47ed50d38d61660c96a2ac07e537cd498f952c0e
aimanshahid93 <108936343+aimanshahid93@users.noreply.github.com>
2022-07-12 02:29:19 +0530

    Add files via upload

---
+++ guvi task 3.js
@@ -0,0 +1,50 @@
+let myResume={
+    "basics": {
+      "name": "aiman shahid",
+      "email": "aimanshahid5003@gamil.com",
+      "phone": 6303259855,
+      "degree": "B.E",
+      "location": {
+        "address": "1-2-340.rose village",
+        "postalCode": 500021,
+        "city": "hyderabad",
+        "state": "Telengana",
+        "country": "India"
+      },
+      
+    "work": [
+      {
+        "company": "Tata consultancy services",
+        "position": "Business Executive",
+        "startDate": "2018",
+        "endDate": "2022",
+      },
+    ],
+    "education": [
+      {
+        "institution": "stanley College Of Engineering and technology for women",
+        "department": "ece",
+        "batch": 2016,
+      }
+    ],
+    "skills": [
+      {
+        "name": "ECE engineer",
+        "level": "fresher",
+        "project": [
+          "automatic detection and destroying of missiles"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "urdu,hindi,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "badminton,debates,",
+      }
+    ]
+  }
+  console.log(myResume);
